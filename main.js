0d4483cca671ec1cde85661c5b4dcea526644af3
Mohamed-gp <mohamedterba6@gmail.com>
2023-10-01 22:59:46 +0100

    IMGS Generators

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-const xml = new XMLHttpRequest
+let btn = document.querySelector(".btn")
+let input = document.querySelector(".input")
+let serbas
 
-console.log(xml)
+let getData = async (url) => {
+    try{
+        fetch(url)
+.then((data) => {return data.json()}).then((data) => {serbas= data,console.log(data)})
+    }catch{
+
+    }finally{
+        btn.addEventListener("click",function(){
+            console.log(input.value)
+            let imgsNum = input.value
+            if (imgsNum > 0 && imgsNum <=11) {
+                let imgs = []
+                for (let i = 0; i < imgsNum; i++) {
+                    imgs[i] = document.createElement("img")
+                    imgs[i].src = data.links.download
+                }
+                for (let i = 0; i < imgsNum; i++) {
+                    document.body.append(imgs[i])
+                }
+            }
+        })
+    }
+    
+
+}
+
+
+getData("https://api.unsplash.com/photos/?client_id=zaeGjV1MvXqTe0D-EWCIUE7OfOfOQjxcJ2cBRi36AoI")
